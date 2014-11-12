d8ec1e89b5d3beea24e8b4e76989102030d039b7
Adam <koreanguy515@yahoo.com>
2014-11-12 16:54:09 +0000

    add votes model and associations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def index
+    @ideas = Idea.all
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141105175857) do
+ActiveRecord::Schema.define(version: 20141112164541) do
 
   create_table "ideas", force: true do |t|
     t.integer  "user_id"
@@ -39,5 +39,13 @@ ActiveRecord::Schema.define(version: 20141105175857) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "votes", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "idea_id"
+    t.boolean  "up"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
